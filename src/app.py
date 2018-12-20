fa440eed61383bc950c0b695ea95f0f2d4f9673f
Konstantin Makarychev <mrsimpson@inbox.ru>
2018-12-21 02:05:22 +0300

    The last commit

--- src/app.py
+++ src/app.py
@@ -80,7 +80,7 @@ def _event_handler(event_type, slack_event):
             'Other': other
         }
         print(ml_request.text)
-        dialect = intent_labels.get(ml_request.json().get("intent"))
+        dialect = intent_labels.get(ml_request.json().get("intent")[0])
 
         dialect(user_id, channel)
 
