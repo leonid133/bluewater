9a2a4a5faa8e52393d896332aae7dcd4841d8e3a
Konstantin Makarychev <mrsimpson@inbox.ru>
2018-12-21 03:30:47 +0300

    Persist queue

--- src/queue.py
+++ src/queue.py
@@ -1,25 +1,42 @@
 # -*- coding: utf-8 -*-
+import redis
+import json
 
 class DummyToiletQueue:
 
     def __init__(self):
-        self.q = []
+        self.redis = redis.Redis(host='bluewater-redis-master', port=6379)
 
     def add(self, id):
-        self.q.append(id)
+        q = self.__get()
+        q.append(id)
+        self.__set(q)
 
     def get_my_status(self, id):
+        q = self.__get()
         try:
-            ind = self.q.index(id)
+            ind = q.index(id)
             return ind
         except ValueError:
             return -1
 
     def remove(self):
         try:
-            return self.q.pop(0)
+            q = self.redis.__get()
+            value = q.pop(0)
+            self.redis.__set(q)
         except IndexError:
             return -1
 
     def size(self):
-        return len(self.q)
+        q = self.redis.__get()
+        return len(q)
+
+    def __set(self, q):
+        self.redis.set('state', json.dumps(q))
+
+    def __get(self):
+        try:
+            return json.loads(self.redis.get('state'))
+        except:
+            return []
