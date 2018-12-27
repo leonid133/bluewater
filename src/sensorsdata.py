a52c4e33f3079e733c7f150051862f88477da329
lblokhin <lenin133@yandex.ru>
2018-12-27 09:09:32 +0300

    arrrr

--- src/sensorsdata.py
+++ src/sensorsdata.py
@@ -29,7 +29,7 @@ class SensorsData:
     def get(self, offset, limit, pattern):
         try:
             result = {}
-            keys = self.scan_keys(pattern=pattern)[offset:offset+limit]
+            keys = self.scan_keys(pattern=pattern)[int(offset):int(offset+limit)]
             for key in keys:
                 result[key] = (self.redis.hgetall(key))
 
